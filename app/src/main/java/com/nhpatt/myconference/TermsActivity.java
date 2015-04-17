6b6397843557d8ba50a600e002338e73ba81ffc8
Javier Gamarra <nhpatt@gmail.com>
2015-04-17 08:11:06 +0200

    terms activity with card view and scroll view

---
+++ app/src/main/java/com/nhpatt/myconference/TermsActivity.java
@@ -0,0 +1,16 @@
+package com.nhpatt.myconference;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Javier Gamarra
+ */
+public class TermsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.terms);
+    }
+}
