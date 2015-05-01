bbf394183bfef93079e51f176cb4d26ed1ddb531
Javier Gamarra <nhpatt@gmail.com>
2015-05-02 01:35:56 +0200

    clearing dashboard, we are not going to use that layout (yet)

--- app/src/main/java/com/nhpatt/myconference/DashboardActivity.java
+++ app/src/main/java/com/nhpatt/myconference/DashboardActivity.java
@@ -1,30 +1,19 @@
 package com.nhpatt.myconference;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import com.nhpatt.myconference.entities.Talk;
 
 
 /**
  * @author Javier Gamarra
  */
-public class DashboardActivity extends Activity implements View.OnClickListener {
+public class DashboardActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
 
-        findViewById(R.id.first_row).setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra("objecto", new Talk());
-        startActivity(intent);
-    }
 }
