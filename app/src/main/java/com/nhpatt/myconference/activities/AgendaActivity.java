0c75b4fd5a07d661c65a47d0b20409705bf3519d
Javier Gamarra <nhpatt@gmail.com>
2015-05-02 09:38:32 +0200

    refactoring layout name to agenda from dashboard

--- app/src/main/java/com/nhpatt/myconference/activities/AgendaActivity.java
+++ app/src/main/java/com/nhpatt/myconference/activities/AgendaActivity.java
@@ -29,7 +29,7 @@ public class AgendaActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dashboard);
+        setContentView(R.layout.agenda);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.talks);
 
