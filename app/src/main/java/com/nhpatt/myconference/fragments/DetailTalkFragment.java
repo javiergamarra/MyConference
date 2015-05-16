7c043f92679deb6b7588e902dc56283c8200de9d
Javier Gamarra <nhpatt@gmail.com>
2015-05-16 09:10:15 +0200

    example of xml fragment

---
+++ app/src/main/java/com/nhpatt/myconference/fragments/DetailTalkFragment.java
@@ -0,0 +1,18 @@
+package com.nhpatt.myconference.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nhpatt.myconference.R;
+
+public class DetailTalkFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.detail_talk, container, false);
+    }
+}
